d85bbcf89a6cc272f6c5338eacf6be9ee4d39540
KoatKoetl <stas20027@outlook.com>
2025-09-16 13:16:14 +0300

    (Feature) - add main installations and configs

--- Addtelligent-intern-programm/vite.config.ts
+++ Addtelligent-intern-programm/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
+import checker from "vite-plugin-checker";
+import inspect from "vite-plugin-inspect";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    checker({ biome: true }),
+    inspect({}),
+  ],
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: false,
+        beautify: false,
+      },
+    },
+  },
+});
