ef0c3b295c5592e90dbd81c69cdcde3d5f390c82
KoatKoetl <stas20027@outlook.com>
2025-09-16 20:38:22 +0300

    (Feature) - add routes to homepage, login and register

--- Addtelligent-intern-programm/src/routes/routes.tsx
+++ Addtelligent-intern-programm/src/routes/routes.tsx
@@ -1,9 +1,13 @@
 // src/RoutesComponent.tsx
 import { Route, Routes } from "react-router";
+import { HomePage } from "../pages/HomePage";
 
 const RoutesComponent = () => {
 	return (
 		<Routes>
+			<Route path="/" element={<HomePage />} />
+			<Route path="/login" element={<h1>Login</h1>} />
+			<Route path="/register" element={<h1>Register</h1>} />
 			<Route path="/news" element={<h1>News</h1>} />
 			<Route path="/news/:id" element={<h1>News detail</h1>} />
 		</Routes>
