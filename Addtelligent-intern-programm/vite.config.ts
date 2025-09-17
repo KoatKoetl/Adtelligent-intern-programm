9f932a9d706d90a305e98903ac3248b7b163680f
KoatKoetl <stas20027@outlook.com>
2025-09-17 14:18:07 +0300

    (NPM) - install image optimizer

--- Addtelligent-intern-programm/vite.config.ts
+++ Addtelligent-intern-programm/vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import inspect from "vite-plugin-inspect";
 import svgr from "vite-plugin-svgr";
 
@@ -13,6 +14,50 @@ export default defineConfig({
 		svgr(),
 		checker({ biome: true }),
 		inspect({}),
+		ViteImageOptimizer({
+			logStats: true,
+			ansiColors: true,
+			test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+			exclude: undefined,
+			include: undefined,
+			includePublic: true,
+			svg: {
+				multipass: true,
+				plugins: [
+					{
+						name: "preset-default",
+						params: {
+							overrides: {
+								cleanupNumericValues: false,
+								cleanupIds: {
+									minify: false,
+									remove: false,
+								},
+								convertPathData: false,
+							},
+						},
+					},
+					"sortAttrs",
+					{
+						name: "addAttributesToSVGElement",
+						params: {
+							attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+						},
+					},
+				],
+			},
+			jpeg: {
+				quality: 100,
+			},
+			jpg: {
+				quality: 100,
+			},
+			webp: {
+				lossless: true,
+			},
+			cache: false,
+			cacheLocation: undefined,
+		}),
 	],
 	build: {
 		minify: "terser",
