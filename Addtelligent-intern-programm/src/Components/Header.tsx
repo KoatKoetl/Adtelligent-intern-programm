902067e0be1a334c469430aa58f1ae349c0037fa
Stanislav Suschevici <stas20027@outlook.com>
2025-09-23 02:05:28 +0300

    (FIX) - use new function to check user auth status

--- Addtelligent-intern-programm/src/Components/Header.tsx
+++ Addtelligent-intern-programm/src/Components/Header.tsx
@@ -1,15 +1,13 @@
 import { Link } from "react-router";
 import ViteLogo from "../assets/vite.svg?react";
 import { useLogout } from "../hooks/useLogout";
-import { useUser } from "../hooks/useUser";
+import { useAuthStatus } from "../hooks/useUser";
 import DefaultButton from "./DefaultButton";
 import { ThemeToggleButton } from "./Providers/ThemeToggleButton";
 
 const Header = () => {
-	const { data: user } = useUser();
+	const { isAuthenticated } = useAuthStatus();
 	const { mutate, isPending } = useLogout();
-
-	const isLoggedIn = !!user;
 
 	return (
 		<header className="sticky top-0 z-1 border-b-1 bg-white border-gray-100 shadow-md dark:bg-dark-theme-800 text-black dark:text-white dark:border-gray-800">
@@ -20,7 +18,7 @@ const Header = () => {
 					</Link>
 				</div>
 
-				{!isLoggedIn ? (
+				{!isAuthenticated ? (
 					<div className="flex gap-1">
 						<Link to="/login">
 							<DefaultButton>Login</DefaultButton>
